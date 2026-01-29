7a380a3873eaac4f96f835eadf404d6705e82b4d
vaseem-ismail <canvas9787839798@gmail.com>
2026-01-29 22:58:26 +0530

    main and requirements added

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ async def translate(data: PageTranslateRequest):
     return JSONResponse(content={"translated": translated_list})
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", port=8000, reload=True)
