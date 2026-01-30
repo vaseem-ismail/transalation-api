21d0f729682c2f2195a14382851b0d3d8fd7e0d2
vaseem-ismail <canvas9787839798@gmail.com>
2026-01-30 11:24:16 +0530

    fixed almost one more time

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from googletrans import Translator
+from deep_translator import GoogleTranslator
 import uvicorn
 from typing import List, Optional
 
@@ -18,18 +18,19 @@ async def index():
 
 @app.post("/translate-page")
 async def translate(data: PageTranslateRequest):
-    translator = Translator()
-
     translated_list = []
+
     for p in data.pageText:
         if p.strip():
-            translated_text = translator.translate(p, src=data.sourceLang, dest=data.targetLang).text
-            translated_list.append(translated_text)
+            translated = GoogleTranslator(
+                source=data.sourceLang,
+                target=data.targetLang
+            ).translate(p)
+            translated_list.append(translated)
         else:
             translated_list.append("")
 
-    print("Translated List:", translated_list)
-    return JSONResponse(content={"translated": translated_list})
+    return {"translated": translated_list}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
